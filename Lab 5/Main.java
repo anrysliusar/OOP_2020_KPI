321ff0830e01ccc678502f347714d696a18b33e0
Andrew Sliusarenko <59937048+anrysliusar@users.noreply.github.com>
2020-06-02 11:33:31 +0300

    Create Main.java

---
+++ Lab 5/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Text text = new Text("ппваыарагыпаоыпаопырап ыаиыоавыаы прпрп");
+        System.out.println(text);
+        System.out.println(text.findLongestPalindrome());
+    }
+}
