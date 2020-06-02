d8246765a25d6ab07122f355c5b55b668f1a03d0
Andrew Sliusarenko <59937048+anrysliusar@users.noreply.github.com>
2020-06-02 13:21:10 +0300

    Add files via upload

---
+++ lab_2_oop/src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final int a = 5;
+        long SumMax = 0;
+        long SumMin = 0;
+        long minValue;
+        long maxValue;
+        long[][] matrix = new long[][]
+                {
+                        {8, 3, 4, 12},
+                        {1, 6, 9, 11},
+                        {2, 7, 5, 10},
+                        {4, 8, 1, 15}
+                };
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println();
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+        System.out.println("\n");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] *= a;
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println();
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+        }
+        System.out.println("\n");
+
+        for (int j = 0; j < 4; j++) {
+            if ((j + 1) % 2 == 1) {
+                minValue = Long.MAX_VALUE;
+                for (int i = 0; i < 4; i++) {
+                    minValue = (long) Math.min(minValue, matrix[i][j]);
+                }
+                SumMin += minValue;
+            }
+
+            if ((j + 1) % 2 == 0) {
+                maxValue = Long.MIN_VALUE;
+                for (int i = 0; i < 4; i++) {
+                    maxValue = (long) Math.max(maxValue, matrix[i][j]);
+                }
+                SumMax += maxValue;
+            }
+
+        }
+
+        System.out.printf("%1s - сума найменших елементів в стовпцях матриці з непарними номерами\n", SumMin);
+        System.out.printf("%1s - сума найбільших елементів в стовпцях матриці з парними номерами ", SumMax);
+    }
+}
