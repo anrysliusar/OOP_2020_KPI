f6f07cbe2f683872f1e6a0707e57974c6f24060c
Andrew Sliusarenko <59937048+anrysliusar@users.noreply.github.com>
2020-06-06 22:12:05 +0300

    Update Main.java

--- lab_5_oop/src/com/company/Main.java
+++ lab_5_oop/src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 //
 
-        Text text = new Text("ппваыарагыпаоыпаопырап ыаиыоавыаы прпрп");
+        Text text = new Text("English texts for beginners to practice reading and        assa foooooof         comprehension online and for free.");
         System.out.println(text);
         System.out.println(text.findLongestPalindrome());
     }
