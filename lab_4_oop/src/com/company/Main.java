9cc5d56041850de550d8a2c3d35a80fd348f6469
Andrew Sliusarenko <59937048+anrysliusar@users.noreply.github.com>
2020-08-07 00:27:10 +0300

    Update Main.java

--- lab_4_oop/src/com/company/Main.java
+++ lab_4_oop/src/com/company/Main.java
@@ -14,8 +14,8 @@ public class Main {
         cosm[1] = new Cosmetics("Mass market", "for hair", "shampoo", "L’Oreal", 240);
         cosm[2] = new Cosmetics("Professional", "face", "eyebrow pencil", "Kryolan", 120);
         cosm[3] = new Cosmetics("LUX", "for skin", "conditioner", "Guerlain", 170);
-//        planes[3] = new Airplane("Antonov An-225", 800, 15400, 250000, 1300);
-//        Arrays.sort(planes, Comparator.comparing(Airplane::getModel));
+
+
         Arrays.sort(cosm, Comparator.comparing(Cosmetics::getName).reversed().thenComparing(Cosmetics::getWeight));
         for (Cosmetics cos : cosm) {
             System.out.println(cos);
